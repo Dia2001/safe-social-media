8600d638bd01eef7d5db53ab380c96f28a56cd5a
nguyenvandiatlvn3@gmail.com <nguyenvandiatlvn3@gmail.com>
2023-05-21 20:55:36 +0700

    fix: image large post

--- client/src/components/LargePost.jsx
+++ client/src/components/LargePost.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import Image from '../assets/images/image-post.jpg';
-import Avatar from '../assets/images/anhdaidien.jpeg';
 import { MdOutlineClose, MdOutlineFavorite } from 'react-icons/md';
 import isAuthorize from "../utils/isAuthorize";
 import getUser from "../utils/getUser";
@@ -10,6 +9,7 @@ import config from "../config";
 const LargePost = ({ close, item }) => {
     const [auth, SetAuth] = useState(false)
     const [user, SetUser] = useState('');
+    const [avatar, SetAvatar] = useState('');
     useEffect(
         () => {
             var isAuth = isAuthorize();
@@ -18,13 +18,14 @@ const LargePost = ({ close, item }) => {
                 (async () => {
                     const getNameUser = await getUser(getCurrentUserId());
                     SetUser(getNameUser.name);
+                    SetAvatar(getNameUser.avatar);
                 })();
             }
         }, []
     )
     return <div className="absolute top-[-120px] left-0 w-full h-[150%] bg-BlackCool/25 ">
         <div className="relative top-[120px] mx-auto mt-[180px] w-[506px] rounded-md overflow-hidden shadow-md z-50">
-            <img src={Avatar} className="rounded-full w-[63px] h-[63px] shadow-sm object-conver absolute top-4 left-4" />
+            <img src={avatar} className="rounded-full w-[63px] h-[63px] shadow-sm object-conver absolute top-4 left-4" />
             <MdOutlineClose onClick={() => close()} size={35} className=" bg-BlackCool/25 p-1 rounded-full text-white absolute right-4 top-4 cursor-pointer" />
             {item.hasOwnProperty('image') ? <img src={`${config.BASE_API_V1}/images/` + item.image} className="w-[506px] object-cover" /> : <img src={bgStatus} className="w-[506px] object-cover" />}
             <div className="p-4 bg-BlackCool/40 w-full text-white absolute bottom-0 left-0">
